e4048b9cadb0128b0f974d59e2ffdae717617587
rama-adi <17980909+rama-adi@users.noreply.github.com>
2023-05-03 14:44:43 +0700

    Initial Commit

---
+++ AFL3POS/AFL3POSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3POSApp.swift
+//  AFL3POS
+//
+//  Created by Rama Adi Nugraha on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3POSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFL3POS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AFL3POS
+//
+//  Created by Rama Adi Nugraha on 03/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
